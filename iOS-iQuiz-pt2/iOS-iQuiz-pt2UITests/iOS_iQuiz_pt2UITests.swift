1ddb618277b033ca11654f979c6e29334bf0d272
Morgan Evans <evansmorg@gmail.com>
2015-11-10 23:20:48 -0800

    initial commit

---
+++ iOS-iQuiz-pt2/iOS-iQuiz-pt2UITests/iOS_iQuiz_pt2UITests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_iQuiz_pt2UITests.swift
+//  iOS-iQuiz-pt2UITests
+//
+//  Created by Morgan Evans on 11/10/15.
+//  Copyright © 2015 Morgan Evans. All rights reserved.
+//
+
+import XCTest
+
+class iOS_iQuiz_pt2UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
