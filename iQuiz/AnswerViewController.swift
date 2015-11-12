b540f88864b94ca18b278354455c7ec91d50ccb5
Morgan Evans <evansmorg@gmail.com>
2015-11-11 22:23:41 -0800

    add functionality to view controllers

--- iQuiz/AnswerViewController.swift
+++ iQuiz/AnswerViewController.swift
@@ -10,26 +10,28 @@ import UIKit
 
 class AnswerViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    var question:String!
+    var chosenAnswer:String?
+    var correctAnswer:String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        questionLabel.text = "The question: \(question)"
+        answerLabel.text = "Correct Answer: \(correctAnswer!)"
+        
+        if chosenAnswer == correctAnswer {
+            self.view.backgroundColor = UIColor.greenColor()
+        } else {
+            self.view.backgroundColor = UIColor.redColor()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    */
 
 }
--- iQuiz/QuestionViewController.swift
+++ iQuiz/QuestionViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
+    private let ANSWER_SEGUE = "answerSegue"
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerPicker: UIPickerView!
+    
+    var currentQuestionIndex = 0
+    var questions:[String] = []
+    var answers:[String: [String]]? = nil
+    var chosenAnswer: String? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +31,25 @@ class QuestionViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // How many selectors we want
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
+    
+    // How many options we need
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return answers![questions[currentQuestionIndex]]!.count
+    }
+    
+    // What each row will show
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return answers![questions[currentQuestionIndex]]![row]
+    }
+    
+    // What to set as our answer
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        chosenAnswer = answers![questions[currentQuestionIndex]]![row]
+        //answerViewController.chosenAnswer = chosenAnswer
+    }
 
 }
