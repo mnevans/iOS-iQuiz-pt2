9989ef40aea35179282fc17f98418024779f706d
Morgan Evans <evansmorg@gmail.com>
2015-11-15 16:30:54 -0800

    add quiz questions and answers

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -9,23 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let questionSeque = "questionSegue"
+    private let mathQuestions = [
+        Question(question: "8 * 3", answers: ["5", "24", "11"], correctAnswer: "24"),
+        Question(question: "4 - 2", answers: ["2", "6", "8"], correctAnswer: "2"),
+        Question(question: "1 + 6", answers: ["6", "5", "7"], correctAnswer: "7")
+    ]
+
+    private let marvelQuestions = [
+        Question(question: "Superman's real name?", answers: ["Clark Kent", "Spider Man", "Frodo"], correctAnswer: "Clark Kent"),
+        Question(question: "Who is a Marvel Character?", answers: ["Superman", "Your mom", "Spongebob"], correctAnswer: "Superman"),
+        Question(question: "What is the name of another comic brand", answers: ["Dark Horse", "Freebird", "Your mom"], correctAnswer: "Dark Horse")
+    ]
+    private let scienceQuestions = [
+        Question(question: "What is the powerhouse of the cell?", answers: ["Mitochondria", "Your Mom", "Conrad"], correctAnswer: "Mitochondria"),
+        Question(question: "What is the act of energy through light", answers: ["Photosynthesis", "YAH YAH YAH", "Lorde"], correctAnswer: "Photosynthesis"),
+        Question(question: "Are Whales mamal?", answers: ["Yes", "No", "Don't tell me nothing"], correctAnswer: "Yes")
+    ]
+    private var quizzes: [String: [Question]] = ["":[]]
     
-    private let subjects = [
-        "Mathematics", "Marvel Super Heroes", "Science"]
-    
-    let cellTableIdentifier = "CellTableIdentifier"
+    let simpleTableIdentifier = "SimpleTableIdentifier"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        quizzes = [
+            "Mathematics": mathQuestions,
+            "Marvel": marvelQuestions,
+            "Science": scienceQuestions
+        ]
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func settings(sender: UIBarButtonItem) {
+    // Event handler for pressing the settings button
+    // Currently displays a pop up dialog
+    @IBAction func settingsPressed(sender: UIBarButtonItem) {
         let title = "Settings go here"
         let controller = UIAlertController(title: title, message: nil,
             preferredStyle: .Alert)
@@ -36,51 +58,50 @@ class ViewController: UIViewController {
         presentViewController(controller, animated: true, completion: nil)
     }
     
+    // Number of mandatory rows
     func tableView(tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int {
-            return subjects.count
+            return quizzes.count
     }
     
+    // Make tablecells
     func tableView(tableView: UITableView,
         cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            var cell = tableView.dequeueReusableCellWithIdentifier(cellTableIdentifier)
+            var cell = tableView.dequeueReusableCellWithIdentifier(simpleTableIdentifier)
                 as UITableViewCell!
             if (cell == nil) {
-                cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: cellTableIdentifier)
+                cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: simpleTableIdentifier)
             }
             
-            let image = UIImage(named: "quiz_img")
+            let image = UIImage(named: "star")
             cell!.imageView?.image = image
-            let highlightedImage = UIImage(named: "quiz_img2")
+            let highlightedImage = UIImage(named: "star2")
             cell!.imageView?.highlightedImage = highlightedImage
             
-            cell!.textLabel?.text = subjects[indexPath.row]
+            cell!.textLabel?.text = Array(quizzes.keys)[indexPath.row]
             cell!.textLabel?.font = UIFont .boldSystemFontOfSize(18)
-            cell!.detailTextLabel?.text = "Quizzes quizzes are so fun, quizzes are for everyone!"
-            cell!.detailTextLabel?.font = UIFont.systemFontOfSize(12)
+            //cell!.detailTextLabel?.text = "Hella single-origin coffee intelligentsia, plaid trust fund keffiyeh 8-bit."
+            //cell.detailTextLabel?.font = UIFont.systemFontOfSize(12)
             return cell!
     }
     
-    
+    // Once we have selected a table cell
     func tableView(tableView: UITableView,
         didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            let cell = tableView.cellForRowAtIndexPath(indexPath)
+            performSegueWithIdentifier(questionSeque, sender: cell)
             tableView.deselectRowAtIndexPath(indexPath, animated: true)
-            let rowValue = subjects[indexPath.row]
-            let message = "You picked \(rowValue)"
-            
-            let controller = UIAlertController(title: "OK",
-                message: message, preferredStyle: .Alert)
-            let action = UIAlertAction(title: "Yes",
-                style: .Default, handler: nil)
-            controller.addAction(action)
-            
-            presentViewController(controller, animated: true, completion: nil)
     }
     
-    func tableView(tableView: UITableView,
-        heightForRowAtIndexPath indexPath: NSIndexPath)
-        -> CGFloat {
-            return indexPath.row == 0 ? 120 : 70
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if segue.identifier == questionSeque {
+            let cell = sender as! UITableViewCell
+            let key = cell.textLabel?.text
+            let problems = quizzes[key!]
+            let questionViewController = segue.destinationViewController as! QuestionViewController
+            
+            questionViewController.questions = problems!
+        }
     }
 }
 
